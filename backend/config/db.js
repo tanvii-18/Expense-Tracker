609864ef06b92aa6f07cc810460d6f936ca50fbe
tanvii-18 <8438tanvipatel@gmail.com>
2026-06-15 18:41:14 +0530

    15-6-26

---
+++ backend/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27018/expense-tracker");
+  } catch (error) {
+    return res.status(500).json({
+      status: false,
+      message: "error in connecting database",
+      error: error.message,
+    });
+  }
+};
