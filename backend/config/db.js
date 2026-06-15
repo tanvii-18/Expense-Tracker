f15bc9c65ec34d59b5959409d9c5837b6b3cf28e
tanvii-18 <8438tanvipatel@gmail.com>
2026-06-15 18:52:48 +0530

    .....

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect("mongodb://localhost:27018/expense-tracker");
+    console.log("db connected!");
   } catch (error) {
     return res.status(500).json({
       status: false,
--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,10 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
+
+const app = express();
+
+connectDB();
+
+app.listen(4000, () => {
+  console.log("server started on http://localhost:4000");
+});
