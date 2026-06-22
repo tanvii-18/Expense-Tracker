08f67ef1d9a9fa904e89e0e64f700493ff528810
tanvii-18 <8438tanvipatel@gmail.com>
2026-06-22 13:44:32 +0530

    22-6-26

--- backend/models/expenseModel.js
+++ backend/models/expenseModel.js
@@ -1,5 +1,38 @@
 import mongoose from "mongoose";
 
-const expenseSchema = new mongoose.Schema({});
+const expenseSchema = new mongoose.Schema(
+  {
+    expenseName: {
+      type: String,
+      required: true,
+    },
+    expenseAmt: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      enum: [
+        "Food",
+        "Transportation",
+        "Entertainment",
+        "Shopping",
+        "Utilities",
+        "Healthcare",
+        "Other",
+      ],
+    },
+    description: {
+      type: String,
+      trim: true,
+      maxlength: 255,
+    },
+    expenseType: {
+      type: String,
+      enum: ["Needs", "Wants", "Savings", "Investment"],
+    },
+  },
+  { timestamps: true },
+);
 
 export const expenseCollection = mongoose.model("expenseSchema", expenseSchema);
